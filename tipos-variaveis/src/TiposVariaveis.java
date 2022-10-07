0c7844f2902409d5f631970fd6345724e2b6fbd2
Aron <aronf.ramalho@gmail.com>
2022-10-07 20:34:32 -0300

    Anotações sobre alguns tipos primitivos.

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte byteValorMinimo = -128;
+        byte byteValorMaximo = 127;
+
+        short shortValorMinimo = -32768;
+        short shortValorMaximo = 32767;
+
+        int intValorMinimo = -2147483648;
+        int intValorMaximo = 2147483647;
+
+        long longValorMinimo = -9223372036854775808L;
+        long longValorMaximo = 9223372036854775807L;
+
+        // Double é mais comum que Float para números fracionados pois Float é só mantem uma precisão decimal entre 6 e 7 dígitos.
+        // Após um número Long é necessário colocar um L no fim e após um número Float é necessário colocar um F no fim.
+
+        System.out.print("O valor minimo para byte é: " + byteValorMinimo + " E o máximo é: " + byteValorMaximo + " O valor minimo para short é:  "
+         + shortValorMinimo + " E o máximo é: " + shortValorMaximo + " O valor minimo para int é:  " + intValorMinimo
+          + " E o máximo é: " + intValorMaximo + " O valor minimo para long é:  " + longValorMinimo + " E o máximo é: " + longValorMaximo);
+
+
     }
 }
